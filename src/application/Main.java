e3feacb3c0c0107b054a786cca0fce208054aa7d
HarrySchreiber <44842528+HarrySchreiber@users.noreply.github.com>
2020-03-26 17:47:05 -0400

    Added col and row constraints for settings page

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,26 +17,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//Grid for Simulation Screen
-			GridPane simulationScreenLayout = new GridPane();
-			simulationScreenLayout.gridLinesVisibleProperty(); //TODO: Remove, for testing
-			
-			//Column Constraints for Simulation Screen
-			ColumnConstraints c1 = new ColumnConstraints();
-			c1.setPercentWidth(25);	//25% of the screen is used up for the side selection bar
-			ColumnConstraints c2 = new ColumnConstraints();
-			c2.setPercentWidth(75);	//75% of the screen is used for the results and data saving section
-			simulationScreenLayout.getColumnConstraints().addAll(c1,c2);
-			//Row Constraints for Simulation Screen
-			RowConstraints r1 = new RowConstraints();
-			r1.setPercentHeight(70);	//70% of the height is set up for the scroll view
-			RowConstraints r2 = new RowConstraints();
-			r2.setPercentHeight(10);	//10% of the height is set up for the run simulation button
-			RowConstraints r3 = new RowConstraints();
-			r3.setPercentHeight(10);	//10% of the height is set up for the edit simulation button
-			RowConstraints r4 = new RowConstraints();
-			r4.setPercentHeight(10);	//10% of the height is set up for the create new simulation button
-			simulationScreenLayout.getRowConstraints().addAll(r1,r2,r3,r4);
+			GridPane simulationScreenLayout = buildSimulationScreen();
 			
 			//Edit Simulation Button on Simulation Screen
 			Button editSimulationBtn = new Button("Edit Simulation");
@@ -44,9 +25,7 @@ public class Main extends Application {
 			editSimulationBtn.setOnAction(e -> primaryStage.setScene(settingsScreen));	//Adds function to the button TODO: Expand function to grab which simulation we need to edit from the radio buttons
 			simulationScreenLayout.add(editSimulationBtn, 0, 2);	//Add the button to the screen
 			
-			//Grid for Settings Screen
-			GridPane settingsScreenLayout = new GridPane();
-			settingsScreenLayout.gridLinesVisibleProperty(); //TODO: Remove for testing
+			GridPane settingsScreenLayout = buildSettingsScreen();
 			
 			//Save Simulation button on Simulation settings screen
 			Button saveSimulationBtn = new Button("Save");
@@ -76,5 +55,61 @@ public class Main extends Application {
 		launch(args);
 	}
 	
+	/**
+	 * A method to extract away some of the setup for the simulation screen
+	 * @return The built simulation screen
+	 */
+	public static GridPane buildSimulationScreen() {
+		//Grid for Simulation Screen
+		GridPane simulationScreenLayout = new GridPane();
+		simulationScreenLayout.gridLinesVisibleProperty(); //TODO: Remove, for testing
+		
+		//Column Constraints for Simulation Screen
+		ColumnConstraints c1 = new ColumnConstraints();
+		c1.setPercentWidth(25);	//25% of the screen is used up for the side selection bar
+		ColumnConstraints c2 = new ColumnConstraints();
+		c2.setPercentWidth(75);	//75% of the screen is used for the results and data saving section
+		simulationScreenLayout.getColumnConstraints().addAll(c1,c2);
+		//Row Constraints for Simulation Screen
+		RowConstraints r1 = new RowConstraints();
+		r1.setPercentHeight(70);	//70% of the height is set up for the scroll view
+		RowConstraints r2 = new RowConstraints();
+		r2.setPercentHeight(10);	//10% of the height is set up for the run simulation button
+		RowConstraints r3 = new RowConstraints();
+		r3.setPercentHeight(10);	//10% of the height is set up for the edit simulation button
+		RowConstraints r4 = new RowConstraints();
+		r4.setPercentHeight(10);	//10% of the height is set up for the create new simulation button
+		simulationScreenLayout.getRowConstraints().addAll(r1,r2,r3,r4);
+		
+		return simulationScreenLayout;
+	}
 	
+	/**
+	 * A method to extract away some of the setup for the settings screen
+	 * @return The built settings screen
+	 */
+	public static GridPane buildSettingsScreen() {
+		//Grid for Settings Screen
+		GridPane settingsScreenLayout = new GridPane();
+		settingsScreenLayout.gridLinesVisibleProperty(); //TODO: Remove for testing
+		
+		//Column Constraints for Settings screen
+		ColumnConstraints c1 = new ColumnConstraints();
+		c1.setPercentWidth(1/3);	//Screen is divided in thirds for different sections
+		ColumnConstraints c2 = new ColumnConstraints();
+		c2.setPercentWidth(1/3);	//Screen is divided in thirds for different sections
+		ColumnConstraints c3 = new ColumnConstraints();
+		c3.setPercentWidth(1/3);	//Screen is divided in thirds for different sections
+		settingsScreenLayout.getColumnConstraints().addAll(c1,c2,c3);
+		//Row Constraints for Settings screen
+		RowConstraints r1 = new RowConstraints();
+		r1.setPercentHeight(10);	//10% of the screen is used for adding a name to the simulation settings
+		RowConstraints r2 = new RowConstraints();
+		r2.setPercentHeight(80);	//80% of the screen is used for the content of the simulation settings
+		RowConstraints r3 = new RowConstraints();
+		r3.setPercentHeight(10);	//10% is used for the save and cancel buttons
+		settingsScreenLayout.getRowConstraints().addAll(r1,r2,r3);
+		
+		return settingsScreenLayout;
+	}
 }
