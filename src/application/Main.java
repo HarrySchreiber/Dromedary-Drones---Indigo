797470d17a0fc1262099601e616556e6a0c43441
HarrySchreiber <44842528+HarrySchreiber@users.noreply.github.com>
2020-03-26 20:46:55 -0400

    Removed an import

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
